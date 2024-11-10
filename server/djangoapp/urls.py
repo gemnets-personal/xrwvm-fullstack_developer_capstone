49014d10c6b0cd5cc22bd5c55deb4ec2dee2222f
gemnets <ssomani.k@gmail.com>
2024-11-10 04:11:18 -0500

    Your commit message

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,6 +6,7 @@ from . import views
 from .models import CarMake, CarModel
 
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
@@ -15,8 +16,7 @@ urlpatterns = [
     path('logout', views.logout_user, name='logout'), 
     path('register', views.register, name='register'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-    
-    
+    path(route='get_dealer', view=views.get_dealerships, name='get_dealers'),  
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
+      
     </Routes>
   );
 }
